fe1f2af07729b293faf80f0dbc5408ca86f9492e
PoliTwit1984 <96096412+PoliTwit1984@users.noreply.github.com>
2022-04-08 18:54:30 -0500

    creating more class methods

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import tweepy
+
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+bearer_token = config.bearer_token
+api = tweepy.API(auth)
+client = tweepy.Client(bearer_token=config.bearer_token)
